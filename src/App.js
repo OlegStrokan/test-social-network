3fb258786f4d2b7d9825d4c5dd0270472933410b
OlegStrokan <oleg14ua71@gmail.com>
2020-11-25 11:28:22 +0100

    Версия сайта без компонентов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app-wrapper'>
+          <header className='header'>
+              <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' alt='logo'/>
+          </header>
+              <nav className='nav'>
+                  <div><a href="#">Profile</a></div>
+                  <div><a href="#">Message</a></div>
+                  <div><a href="#">News</a></div>
+                  <div><a href="#">Music</a></div>
+                  <div><a href="#">Settings</a></div>
+              </nav>
+            <div className="content">
+                <div>
+                    <img src="https://i.imgur.com/ikJDyFT.jpg" alt="main-img"/>
+                </div>
+                <div>
+                    ava + desk
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New posts
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </div>
   );
 }
+
 
 export default App;

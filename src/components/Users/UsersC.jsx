6c3313c69f666f9d938652e672d3e045c8d48d74
Oleh Strokan <oleg14ua71@gmail.com>
2020-12-17 22:02:56 +0100

    Урок 54. componentDidMount

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -4,10 +4,10 @@ import * as axios from 'axios'
 import userPhoto from '../../assets/images/img.jpg'
 
 class Users extends React.Component{
-    constructor(props) {
-        super(props)
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
         })
     }
     render() {
